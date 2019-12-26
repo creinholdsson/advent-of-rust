4b90c6d0793bf8cd644967bbbaded97d6238b8e8
Christofer Reinholdsson <christofer.reinholdsson@gmail.com>
2019-12-26 19:34:32 +0100

    Day 5 Part 1

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -3,27 +3,48 @@ use std::fs;
 use std::io;
 use std::iter::FromIterator;
 
-#[derive(PartialEq)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 enum ParameterMode {
     Position,
-    Immediate
+    Immediate,
+}
+
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Add,
+    Multiply,
+    Input,
+    Output,
+    Unknown,
+    Halt,
 }
 
 #[derive(Debug)]
 struct Operator {
-    op_code: i32,
+    instruction: Instruction,
     param1: ParameterMode,
     param2: ParameterMode,
-    param3: ParameterMode
+    param3: ParameterMode,
 }
 
 impl ParameterMode {
     fn from_char(x: char) -> ParameterMode {
-        println!("evaluating from char");
         match x {
             '1' => ParameterMode::Immediate,
-            _ => ParameterMode::Position
+            _ => ParameterMode::Position,
+        }
+    }
+}
+
+impl Instruction {
+    fn from_i32(x: i32) -> Instruction {
+        match x {
+            1 => Instruction::Add,
+            2 => Instruction::Multiply,
+            3 => Instruction::Input,
+            4 => Instruction::Output,
+            99 => Instruction::Halt,
+            _ => Instruction::Unknown,
         }
     }
 }
@@ -32,46 +53,54 @@ fn get_operator(op_codes: i32) -> Operator {
     let chars: Vec<char> = op_codes.to_string().chars().collect();
 
     let op_code = match chars.len() {
-        
-        2..=1000 => i32::from_str_radix(&String::from_iter(chars[chars.len()-2 as usize..].iter()), 10).unwrap(),
-        _ => chars[chars.len() - 1 as usize].to_digit(10).unwrap() as i32
+        2..=1000 => Instruction::from_i32(
+            i32::from_str_radix(
+                &String::from_iter(chars[chars.len() - 2 as usize..].iter()),
+                10,
+            )
+            .unwrap(),
+        ),
+        _ => Instruction::from_i32(chars[chars.len() - 1 as usize].to_digit(10).unwrap() as i32),
     };
 
     let param1_mode = match chars.len() {
         3..=1000 => ParameterMode::from_char(chars[chars.len() - 3 as usize]),
-        _ => ParameterMode::Position
+        _ => ParameterMode::Position,
     };
 
     let param2_mode = match chars.len() {
         4..=1000 => ParameterMode::from_char(chars[chars.len() - 4 as usize]),
-        _ => ParameterMode::Position
+        _ => ParameterMode::Position,
     };
 
     let param3_mode = match chars.len() {
         5..=1000 => ParameterMode::from_char(chars[chars.len() - 5 as usize]),
-        _ => ParameterMode::Position
+        _ => ParameterMode::Position,
     };
 
-    Operator { op_code, param1: param1_mode, param2: param2_mode, param3: param3_mode}
+    Operator {
+        instruction: op_code,
+        param1: param1_mode,
+        param2: param2_mode,
+        param3: param3_mode,
+    }
 }
 
 fn get_mutated_sequence(v: &mut Vec<i32>) -> &mut Vec<i32> {
     let mut curr_op_code = get_operator(v[0]);
-    println!("{:?}", curr_op_code);
     let mut current_pos = 0;
 
-    while curr_op_code.op_code != 99 {
+    while curr_op_code.instruction != Instruction::Halt {
         let mut next_pos: usize = current_pos;
-        match curr_op_code.op_code {
-            1 => next_pos = add_instruction(v, current_pos, &curr_op_code),
-            2 => next_pos = mult_instruction(v, current_pos, &curr_op_code),
-            3 => next_pos = input_instruction(v, current_pos),
-            4 => next_pos = output_instruction(v, current_pos),
-            _ => println!("unknown op code {}", curr_op_code.op_code),
+        match curr_op_code.instruction {
+            Instruction::Add => next_pos = add_instruction(v, current_pos, &curr_op_code),
+            Instruction::Multiply => next_pos = mult_instruction(v, current_pos, &curr_op_code),
+            Instruction::Input => next_pos = input_instruction(v, current_pos),
+            Instruction::Output => next_pos = output_instruction(v, current_pos),
+            _ => println!("unknown op code {:?}", curr_op_code.instruction),
         }
         current_pos = next_pos;
         curr_op_code = get_operator(v[current_pos]);
-        println!("{:?}", curr_op_code);
     }
 
     v
@@ -80,16 +109,16 @@ fn get_mutated_sequence(v: &mut Vec<i32>) -> &mut Vec<i32> {
 fn add_instruction(v: &mut Vec<i32>, current_pos: usize, operator: &Operator) -> usize {
     let l_value = match operator.param1 {
         ParameterMode::Position => v[v[current_pos + 1] as usize],
-        ParameterMode::Immediate => v[current_pos + 1]
+        ParameterMode::Immediate => v[current_pos + 1],
     };
-     
+
     let r_value = match operator.param2 {
         ParameterMode::Position => v[v[current_pos + 2] as usize],
-        ParameterMode::Immediate => v[current_pos + 2]
+        ParameterMode::Immediate => v[current_pos + 2],
     };
     let output_pos = match operator.param3 {
         ParameterMode::Position => v[current_pos + 3] as usize,
-        ParameterMode::Immediate => v[current_pos + 3] as usize
+        ParameterMode::Immediate => v[current_pos + 3] as usize,
     };
     v[output_pos] = l_value + r_value;
     current_pos + 4
@@ -98,16 +127,16 @@ fn add_instruction(v: &mut Vec<i32>, current_pos: usize, operator: &Operator) ->
 fn mult_instruction(v: &mut Vec<i32>, current_pos: usize, operator: &Operator) -> usize {
     let l_value = match operator.param1 {
         ParameterMode::Position => v[v[current_pos + 1] as usize],
-        ParameterMode::Immediate => v[current_pos + 1]
+        ParameterMode::Immediate => v[current_pos + 1],
     };
-     
+
     let r_value = match operator.param2 {
         ParameterMode::Position => v[v[current_pos + 2] as usize],
-        ParameterMode::Immediate => v[current_pos + 2]
+        ParameterMode::Immediate => v[current_pos + 2],
     };
     let output_pos = match operator.param3 {
         ParameterMode::Position => v[current_pos + 3] as usize,
-        ParameterMode::Immediate => v[current_pos + 3] as usize
+        ParameterMode::Immediate => v[current_pos + 3] as usize,
     };
     v[output_pos] = l_value * r_value;
     current_pos + 4
@@ -119,17 +148,26 @@ fn input_instruction(v: &mut Vec<i32>, current_pos: usize) -> usize {
     let output_pos = v[current_pos + 1] as usize;
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            let input  = i32::from_str_radix(&input.trim(), 10).unwrap();
+            let input = i32::from_str_radix(&input.trim(), 10).unwrap();
             v[output_pos] = input;
-        },
-        Err(error) => panic!(error)
+        }
+        Err(error) => panic!(error),
     }
-    current_pos + 1
+    current_pos + 2
 }
 
-fn output_instruction(v: &Vec<i32>, current_pos: usize) -> usize {
-    println!("Program output: {}", v[current_pos + 1]);
-    current_pos + 1
+fn output_instruction(v: &[i32], current_pos: usize) -> usize {
+    if v[v[current_pos + 1] as usize] > 0 {
+        println!(
+            "Possible error: {} (current pos: {})",
+            v[v[current_pos + 1] as usize],
+            current_pos
+        );
+    } else {
+        println!("Output: {}", v[v[current_pos + 1] as usize]);
+    }
+
+    current_pos + 2
 }
 
 fn main() {
@@ -183,8 +221,26 @@ fn test_input_sequence4() {
 fn test_op_code_parser() {
     let op = get_operator(1002);
 
-    assert_eq!(2, op.op_code);
+    assert_eq!(Instruction::Multiply, op.instruction);
     assert_eq!(ParameterMode::Position, op.param1);
     assert_eq!(ParameterMode::Immediate, op.param2);
     assert_eq!(ParameterMode::Position, op.param3);
+}
+
+#[test]
+fn test_new_sequence() {
+    let mut input = vec![1002, 4, 3, 4, 33];
+    println!("{:?}", get_mutated_sequence(&mut input));
+
+    assert_eq!(99, input[4]);
+    assert_eq!(input, [1002, 4, 3, 4, 99]);
+}
+
+#[test]
+fn test_new_sequence2() {
+    let mut input = vec![1102, 3, 33, 4, 33];
+    println!("{:?}", get_mutated_sequence(&mut input));
+
+    assert_eq!(99, input[4]);
+    assert_eq!(input, [1102, 3, 33, 4, 99]);
 }
